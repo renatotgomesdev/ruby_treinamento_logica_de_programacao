820d2b6b8c21722fee783207de6c3cc6f6ebcbe2
renatotgomesdev <renatotgomes.dev@gmail.com>
2023-05-31 05:51:32 -0300

    Criação do primeiro exercício em Ruby

---
+++ exercicio-01.rb
@@ -0,0 +1,16 @@
+=begin
+Exercício da aula 2 - Variáveis - aos 37 minutos do vídeo
+
+Dado que eu tenha uma variável com o número 360, qual seria o resultado da soma
+da variável b que tem armazenado 44 com a variável anterior?
+=end
+
+a = 360
+b = 44
+resultado = a + b
+puts "-" * 60
+puts "Os valor da variável a é: " + a.to_s
+puts "Os valor da variável b é: " + b.to_s
+puts "-" * 60
+# puts "O resultado da soma das variáveis a + b é igual: " + (a + b).to_s
+puts "O resultado da soma das variáveis a + b é igual: #{resultado}"
