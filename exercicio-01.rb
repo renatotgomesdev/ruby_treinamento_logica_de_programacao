e36c70cb869681bdfecc8580f1076cf4efe9e6c1
renatotgomesdev <renatotgomes.dev@gmail.com>
2023-05-31 05:53:22 -0300

    Refatoração do exercício 01

--- exercicio-01.rb
+++ exercicio-01.rb
@@ -15,9 +15,9 @@ puts "-" * 60
 puts "Cálculo de Desconto sobre o valor total do Pedido"
 puts "-" * 60
 print "\nInforme o valor total do Pedido: R$ "
-valorPedido = gets.to_f
+valorPedido = gets.to_f.round(2)
 print "Informe o percentual (%) de desconto: "
-valorPercentual = gets.to_f
+valorPercentual = gets.to_f.round(2)
 
 percentual = valorPercentual / 100
 valorDesconto = valorPedido * percentual
@@ -25,22 +25,22 @@ valorFinal = valorPedido - valorDesconto
 
 print "\n"
 puts "-" * 60
-puts "Valor do Pedido: R$ #{valorPedido}"
-puts "Desconto: #{valorPercentual}% - R$ #{valorDesconto}"
+puts "Valor do Pedido: R$ #{'%.2f' % valorPedido}"
+puts "Desconto: #{'%.2f' % valorPercentual}% - R$ #{'%.2f' % valorDesconto}"
 puts "-" * 60
-puts "Valor à Receber: R$ #{valorFinal}"
+puts "Valor à Receber: R$ #{'%.2f' % valorFinal}"
 puts "-" * 60
 print "\n\nPagamento em dinheiro ou cartão? (digite d - dinheiro ou c - cartão): "
 formaPagamento = gets.chomp
 
 if formaPagamento == 'd'
     print "\nInforme a quantida em dinheiro entrege pelo cliente: R$ "
-    dinheiroRecebido = gets.to_f
-    puts "\nDinheiro entregue pelo cliente: R$ #{dinheiroRecebido}"
-    puts "Valor cobrado: R$ #{valorFinal}"
+    dinheiroRecebido = gets.to_f.round(2)
+    puts "\nDinheiro entregue pelo cliente: R$ #{'%.2f' % dinheiroRecebido}"
+    puts "Valor cobrado: R$ #{'%.2f' % valorFinal}"
     puts "-" * 60
     troco = dinheiroRecebido - valorFinal
-    puts "Troco à entregar ao cliente: R$ #{troco}"
+    puts "Troco à entregar ao cliente: R$ #{'%.2f' % troco}"
     puts "-" * 60
     puts "\nOperação finalizada."
 else
