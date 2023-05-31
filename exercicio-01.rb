6fe5cba76144e2229cee313aeb9ee42309514cf3
renatotgomesdev <renatotgomes.dev@gmail.com>
2023-05-31 05:52:27 -0300

    Refatoração do exercício 01

--- exercicio-01.rb
+++ exercicio-01.rb
@@ -1,16 +1,13 @@
-=begin
-Exercício da aula 2 - Variáveis - aos 37 minutos do vídeo
-
-Dado que eu tenha uma variável com o número 360, qual seria o resultado da soma
-da variável b que tem armazenado 44 com a variável anterior?
-=end
-
-a = 360
-b = 44
-resultado = a + b
 puts "-" * 60
-puts "Os valor da variável a é: " + a.to_s
-puts "Os valor da variável b é: " + b.to_s
+puts "Operação matemática de soma entre dois números"
+puts "-" * 60
+print "Informe o primeiro número: "
+a = gets.to_i
+print "Informe o segundo número: "
+b = gets.to_i
+
+resultado = a + b
+
 puts "-" * 60
 # puts "O resultado da soma das variáveis a + b é igual: " + (a + b).to_s
-puts "O resultado da soma das variáveis a + b é igual: #{resultado}"
+puts "O resultado da soma é: #{resultado}"
