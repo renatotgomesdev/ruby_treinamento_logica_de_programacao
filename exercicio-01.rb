2ae472e79ad3a5fc0434997ff16974035193bc69
renatotgomesdev <renatotgomes.dev@gmail.com>
2023-05-31 05:53:44 -0300

    Refatoração do exercício 01

--- exercicio-01.rb
+++ exercicio-01.rb
@@ -7,12 +7,16 @@ Faça um software que:
 - Solicite o valor da porcentagem;
 - Mostre o valor do desconto;
 - Mostre o valor com desconto aplicado (valor do pedido - valor do desconto).
+
+### Refatoração - Inclusão de nova função
+Se o valor total com desconto for inferior a R$ 10,00, o seu João irá oferecer um item a mais
+no pedido no valor de R$ 15,00.
 =end
 
 system("cls")
 print "\n"
 puts "-" * 60
-puts "Cálculo de Desconto sobre o valor total do Pedido"
+puts "#### Cálculo de Desconto sobre o valor total do Pedido ####"
 puts "-" * 60
 print "\nInforme o valor total do Pedido: R$ "
 valorPedido = gets.to_f.round(2)
@@ -30,19 +34,71 @@ puts "Desconto: #{'%.2f' % valorPercentual}% - R$ #{'%.2f' % valorDesconto}"
 puts "-" * 60
 puts "Valor à Receber: R$ #{'%.2f' % valorFinal}"
 puts "-" * 60
-print "\n\nPagamento em dinheiro ou cartão? (digite d - dinheiro ou c - cartão): "
+
+valorNovoItem = 0
+
+if valorFinal < 10
+    print "\nGostaria de incluir um novo item em seu pedido (S/N): "
+    novoItem = gets.chomp
+    if novoItem.upcase.strip == "S"
+        print "\n"
+        puts "-" * 60
+        puts "Selecione o seu item adicional"
+        puts "-" * 60
+        puts "\n1 - Batata Frita - R$ 15,00"
+        print "\n2 - Sorvete Gourmet - R$ 20,00\n"
+        print "\n3 - Cancelar item adicional\n\n"
+        opcao = gets.to_f.round(2)
+        if opcao == 1
+            item = "Batata Frita"
+            valorNovoItem = 15
+        elsif opcao == 2
+            item = "Sorvete Gourmet"
+            valorNovoItem = 20
+        else
+            valorNovoItem = 0
+        end
+        valorFinal = valorFinal + valorNovoItem;
+    end
+end
+
+print "\nPagamento em dinheiro ou cartão? (digite D - dinheiro ou C - cartão): "
 formaPagamento = gets.chomp
 
-if formaPagamento == 'd'
+if formaPagamento.upcase.strip == 'D'
+    if valorNovoItem > 0
+        print "\n"
+        puts "-" * 60
+        puts "#### Pedido Atualizado ####"
+        puts "-" * 60
+        puts "Valor do Pedido: R$ #{'%.2f' % valorPedido}"
+        puts "Desconto: #{'%.2f' % valorPercentual}% - R$ #{'%.2f' % valorDesconto}"
+        puts "Acréscimo de 1 #{item}: R$ #{'%.2f' % valorNovoItem}"
+        puts "-" * 60
+        puts "Valor à Receber: R$ #{'%.2f' % valorFinal}"
+        puts "-" * 60
+    end
     print "\nInforme a quantida em dinheiro entrege pelo cliente: R$ "
     dinheiroRecebido = gets.to_f.round(2)
     puts "\nDinheiro entregue pelo cliente: R$ #{'%.2f' % dinheiroRecebido}"
-    puts "Valor cobrado: R$ #{'%.2f' % valorFinal}"
+    print "Valor cobrado: R$ #{'%.2f' % valorFinal}\n\n"
     puts "-" * 60
     troco = dinheiroRecebido - valorFinal
-    puts "Troco à entregar ao cliente: R$ #{'%.2f' % troco}"
+    puts "Devolver ao cliente o troco no valor de: R$ #{'%.2f' % troco}"
     puts "-" * 60
-    puts "\nOperação finalizada."
+    print "\nOperação finalizada."
 else
-    puts "Operação finalizada."
+    print "\n"
+    if valorNovoItem > 0
+        puts "-" * 60
+        puts "#### Pedido Atualizado ####"
+        puts "-" * 60
+        puts "Valor do Pedido: R$ #{'%.2f' % valorPedido}"
+        puts "Desconto: #{'%.2f' % valorPercentual}% - R$ #{'%.2f' % valorDesconto}"
+        puts "Acréscimo de 1 #{item}: R$ #{'%.2f' % valorNovoItem}"
+    end
+    puts "-" * 60
+    puts "Valor Recebido no Cartão: R$ #{'%.2f' % valorFinal}"
+    puts "-" * 60
+    print "\nOperação finalizada."
 end
