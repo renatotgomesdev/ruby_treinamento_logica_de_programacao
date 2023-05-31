18bdb0442842ab4df88d18fba8d8b33936b55cb5
renatotgomesdev <renatotgomes.dev@gmail.com>
2023-05-31 05:53:00 -0300

    Refatoração do exercício 01

--- exercicio-01.rb
+++ exercicio-01.rb
@@ -1,13 +1,48 @@
-puts "-" * 60
-puts "Operação matemática de soma entre dois números"
-puts "-" * 60
-print "Informe o primeiro número: "
-a = gets.to_i
-print "Informe o segundo número: "
-b = gets.to_i
+=begin
+### Exercício Proposto ###
+Seu João é um comerciante e ele precisa de um software que faça o cálculo de porcentagem
+de desconto que ele dará para seus clientes no valor total da compra.
+Faça um software que:
+- Solicite o valor total do pedido;
+- Solicite o valor da porcentagem;
+- Mostre o valor do desconto;
+- Mostre o valor com desconto aplicado (valor do pedido - valor do desconto).
+=end
 
-resultado = a + b
-
+system("cls")
+print "\n"
 puts "-" * 60
-# puts "O resultado da soma das variáveis a + b é igual: " + (a + b).to_s
-puts "O resultado da soma é: #{resultado}"
+puts "Cálculo de Desconto sobre o valor total do Pedido"
+puts "-" * 60
+print "\nInforme o valor total do Pedido: R$ "
+valorPedido = gets.to_f
+print "Informe o percentual (%) de desconto: "
+valorPercentual = gets.to_f
+
+percentual = valorPercentual / 100
+valorDesconto = valorPedido * percentual
+valorFinal = valorPedido - valorDesconto
+
+print "\n"
+puts "-" * 60
+puts "Valor do Pedido: R$ #{valorPedido}"
+puts "Desconto: #{valorPercentual}% - R$ #{valorDesconto}"
+puts "-" * 60
+puts "Valor à Receber: R$ #{valorFinal}"
+puts "-" * 60
+print "\n\nPagamento em dinheiro ou cartão? (digite d - dinheiro ou c - cartão): "
+formaPagamento = gets.chomp
+
+if formaPagamento == 'd'
+    print "\nInforme a quantida em dinheiro entrege pelo cliente: R$ "
+    dinheiroRecebido = gets.to_f
+    puts "\nDinheiro entregue pelo cliente: R$ #{dinheiroRecebido}"
+    puts "Valor cobrado: R$ #{valorFinal}"
+    puts "-" * 60
+    troco = dinheiroRecebido - valorFinal
+    puts "Troco à entregar ao cliente: R$ #{troco}"
+    puts "-" * 60
+    puts "\nOperação finalizada."
+else
+    puts "Operação finalizada."
+end
